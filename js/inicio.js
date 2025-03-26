2876a04b0de7589c36f1dd2b6d4e89ae1085eb4d
Henrique Ferreira Polizer <polizerrickk@gmail.com>
2025-03-26 17:35:46 -0300

    update: atuaçização n formato para .webp

--- js/inicio.js
+++ js/inicio.js
@@ -1,12 +1,1 @@
-function enviarWhatsApp( event ) {
-
-    event.preventDefault();
-    
-    let numero   = "5544997687380";
-    let nome     = document.getElementById( "name-input" ).value;
-    let mensagem = document.getElementById( "message" ).value;
-    let textoFormatado = `Olá, me chamo ${nome}.\n${mensagem}`;
-    let url = `https://wa.me/${numero}?text=${encodeURIComponent(textoFormatado)}`;
-
-    window.open(url, "_blank");
-}
+function enviarWhatsApp(e){e.preventDefault();let n=document.getElementById("name-input").value,t=document.getElementById("message").value,a=`https://wa.me/5544997687380?text=${encodeURIComponent(`Olá, me chamo ${n}.\n${t}`)}`;window.open(a,"_blank")}
