164cfa391358ff292592e55c80daa26d214bd698
Henrique Ferreira Polizer <polizerrickk@gmail.com>
2025-03-27 09:22:52 -0300

    update: desminificando o js

--- js/inicio.js
+++ js/inicio.js
@@ -1,1 +1,12 @@
-function enviarWhatsApp(e){e.preventDefault();let n=document.getElementById("name-input").value,t=document.getElementById("message").value,a=`https://wa.me/5544997687380?text=${encodeURIComponent(`Olá, me chamo ${n}.\n${t}`)}`;window.open(a,"_blank")}
+function enviarWhatsApp( event ) {
+
+    event.preventDefault();
+    
+    let numero   = "5544997687380";
+    let nome     = document.getElementById( "name-input" ).value;
+    let mensagem = document.getElementById( "message" ).value;
+    let textoFormatado = `Olá, me chamo ${nome}.\n${mensagem}`;
+    let url = `https://wa.me/${numero}?text=${encodeURIComponent(textoFormatado)}`;
+
+    window.open(url, "_blank");
+}
